6ba8d9db24582ac434a5818a44c05ad70a223b42
sanjiv kumar <sanjivkumarhzb1427@gmail.com>
2025-11-19 14:02:14 +0530

    bug fixing

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = "https://sanjivworkshop.ddnsfree.com/api/v1/shopping-list/"
-export const RAZORPAY_KEY_ID = 'rzp_test_Rh8c7Ixl9o1vle'
+export const RAZORPAY_KEY_ID = 'sdlkfsldfjld'
